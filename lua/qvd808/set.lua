0c5fe0999da32479c25765d05583491f374923c8
Quang Vinh Dang <dqvinh101@gmail.com>
2024-04-25 16:22:39 -0700

    Update file

--- lua/qvd808/set.lua
+++ lua/qvd808/set.lua
@@ -43,3 +43,13 @@ vim.opt.scrolloff = 20
 
 -- Set highlight on search
 vim.opt.hlsearch = true
+
+-- Autocommand
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {clear = true}),
+	callback =  function ()
+		vim.highlight.on_yank()
+	end,
+})
+
